0dda04d21e5d29ac19a1ab3fada1cf1e77fda36d
Paul Betts <paul@paulbetts.org>
2013-10-08 01:24:38 -0700

    Add the Proletariat version of methods

---
+++ Punchclock/OperationQueueExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punchclock
+{
+    public static class OperationQueueExtensions
+    {
+        public static Task<T> Enqueue<T>(this OperationQueue This, int priority, string key, Func<Task<T>> asyncOperation)
+        {
+            return This.EnqueueObservableOperation(priority, key, () => asyncOperation().ToObservable()).ToTask();
+        }
+
+        public static Task<T> Enqueue<T>(this OperationQueue This, int priority, Func<Task<T>> asyncOperation)
+        {
+            return This.EnqueueObservableOperation(priority, () => asyncOperation().ToObservable()).ToTask();
+        }
+    }
+}
