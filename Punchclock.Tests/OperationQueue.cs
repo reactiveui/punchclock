cefec7cbdf4f02326fc0073dd7e4fcafb50f7506
Paul Betts <paul@paulbetts.org>
2013-10-08 12:11:21 -0700

    Write test to ensure keys are serialized
    Also, stub out some other stuff we want to test

--- Punchclock.Tests/OperationQueue.cs
+++ Punchclock.Tests/OperationQueue.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Subjects;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ReactiveUI;
 using Xunit;
-using System.Reactive.Linq;
 
 namespace Punchclock.Tests
 {
@@ -49,6 +49,75 @@ namespace Punchclock.Tests
 
             subjects[3].OnNext(42); subjects[3].OnCompleted();
             Assert.Equal(new[] { 1, 1, 1, 1, 1, }, outputs.Select(x => x.Count));
+        }
+
+        [Fact]
+        public void KeyedItemsShouldBeSerialized()
+        {
+            var subj1 = new AsyncSubject<int>();
+            var subj2 = new AsyncSubject<int>();
+
+            var subscribeCount1 = 0;
+            var input1Subj = new AsyncSubject<int>();
+            var input1 = Observable.Defer(() => {
+                subscribeCount1++;
+                return input1Subj;
+            });
+            var subscribeCount2 = 0;
+            var input2Subj = new AsyncSubject<int>();
+            var input2 = Observable.Defer(() => {
+                subscribeCount2++;
+                return input2Subj;
+            });
+
+            var fixture = new OperationQueue(2);
+
+            // Block up the queue
+            foreach (var v in new[] { subj1, subj2, }) {
+                fixture.EnqueueObservableOperation(5, () => v);
+            }
+
+            // subj1,2 are live, input1,2 are in queue
+            var out1 = fixture.EnqueueObservableOperation(5, "key", () => input1).CreateCollection();
+            var out2 = fixture.EnqueueObservableOperation(5, "key", () => input2).CreateCollection();
+            Assert.Equal(0, subscribeCount1);
+            Assert.Equal(0, subscribeCount2);
+
+            // Dispatch both subj1 and subj2, we should end up with input1 live, 
+            // but input2 in queue because of the key
+            subj1.OnNext(42); subj1.OnCompleted();
+            subj2.OnNext(42); subj2.OnCompleted();
+            Assert.Equal(1, subscribeCount1);
+            Assert.Equal(0, subscribeCount2);
+            Assert.Equal(0, out1.Count);
+            Assert.Equal(0, out2.Count);
+
+            // Dispatch input1, input2 can now execute
+            input1Subj.OnNext(42); input1Subj.OnCompleted();
+            Assert.Equal(1, subscribeCount1);
+            Assert.Equal(1, subscribeCount2);
+            Assert.Equal(1, out1.Count);
+            Assert.Equal(0, out2.Count);
+
+            // Dispatch input2, everything is finished
+            input2Subj.OnNext(42); input2Subj.OnCompleted();
+            Assert.Equal(1, subscribeCount1);
+            Assert.Equal(1, subscribeCount2);
+            Assert.Equal(1, out1.Count);
+            Assert.Equal(1, out2.Count);
+        }
+
+
+        [Fact]
+        public void NonkeyedItemsShouldRunInParallel()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public void ShutdownShouldSignalOnceEverythingCompletes()
+        {
+            Assert.False(true);
         }
     }
 }
