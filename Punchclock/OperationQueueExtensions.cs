d7a755e680d2418a899daa7e1dfa9c25951da1e9
Paul Betts <paul@paulbetts.org>
2013-10-08 20:37:04 -0700

    Add cancellation for the Working Class

--- Punchclock/OperationQueueExtensions.cs
+++ Punchclock/OperationQueueExtensions.cs
@@ -6,11 +6,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Reactive.Linq;
 using System.Reactive;
+using System.Threading;
+using System.Reactive.Subjects;
 
 namespace Punchclock
 {
     public static class OperationQueueExtensions
     {
+        public static Task<T> Enqueue<T>(this OperationQueue This, int priority, string key, CancellationToken token, Func<Task<T>> asyncOperation)
+        {
+            var cancel = new AsyncSubject<Unit>();
+
+            if (token.IsCancellationRequested) {
+                throw new ArgumentException("Token is already cancelled");
+            }
+
+            token.Register(() => { cancel.OnNext(Unit.Default); cancel.OnCompleted(); });
+            return This.EnqueueObservableOperation(priority, key, cancel, () => asyncOperation().ToObservable()).ToTask(token);
+        }
+
         public static Task<T> Enqueue<T>(this OperationQueue This, int priority, string key, Func<Task<T>> asyncOperation)
         {
             return This.EnqueueObservableOperation(priority, key, Observable.Never<Unit>(), () => asyncOperation().ToObservable()).ToTask();
