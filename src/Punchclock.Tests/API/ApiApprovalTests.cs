4a0d1815e6d2dbef5738d406f04f6d512c4105b9
Chris Pulman <chris.pulman@yahoo.com>
2024-01-01 00:34:24 +0000

    Feature : Add Net 8.0 (#296)

--- src/Punchclock.Tests/API/ApiApprovalTests.cs
+++ src/Punchclock.Tests/API/ApiApprovalTests.cs
@@ -3,88 +3,24 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
-using System;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
-
-using DiffEngine;
-
-using PublicApiGenerator;
-
-using Splat;
-
+using System.Threading.Tasks;
+using VerifyXunit;
 using Xunit;
 
-namespace Punchclock.APITests
+namespace Punchclock.APITests;
+
+/// <summary>
+/// Tests for handling API approval.
+/// </summary>
+[ExcludeFromCodeCoverage]
+[UsesVerify]
+public class ApiApprovalTests
 {
     /// <summary>
-    /// Tests for handling API approval.
+    /// Tests to make sure the punchclock project is approved.
     /// </summary>
-    [ExcludeFromCodeCoverage]
-    public class ApiApprovalTests
-    {
-        private static readonly Regex _removeCoverletSectionRegex = new Regex(@"^namespace Coverlet\.Core\.Instrumentation\.Tracker.*?^}", RegexOptions.Singleline | RegexOptions.Multiline | RegexOptions.Compiled);
-
-        /// <summary>
-        /// Tests to make sure the punchclock project is approved.
-        /// </summary>
-        [Fact]
-        public void PunchclockTests()
-        {
-            CheckApproval(typeof(OperationQueue).Assembly);
-        }
-
-        private static void CheckApproval(Assembly assembly, [CallerMemberName] string memberName = null, [CallerFilePath] string filePath = null)
-        {
-            var targetFrameworkName = Assembly.GetExecutingAssembly().GetTargetFrameworkName();
-
-            var sourceDirectory = Path.GetDirectoryName(filePath);
-
-            var approvedFileName = Path.Combine(sourceDirectory, $"ApiApprovalTests.{memberName}.{targetFrameworkName}.approved.txt");
-            var receivedFileName = Path.Combine(sourceDirectory, $"ApiApprovalTests.{memberName}.{targetFrameworkName}.received.txt");
-
-            if (!File.Exists(receivedFileName))
-            {
-                File.Create(receivedFileName).Close();
-            }
-
-            if (!File.Exists(approvedFileName))
-            {
-                File.Create(approvedFileName).Close();
-            }
-
-            var approvedPublicApi = File.ReadAllText(approvedFileName);
-
-            var generatorOptions = new ApiGeneratorOptions { AllowNamespacePrefixes = new[] { "Akavache" } };
-            var receivedPublicApi = Filter(assembly.GeneratePublicApi(generatorOptions));
-
-            if (!string.Equals(receivedPublicApi, approvedPublicApi, StringComparison.InvariantCulture))
-            {
-                File.WriteAllText(receivedFileName, receivedPublicApi);
-                DiffRunner.Launch(receivedFileName, approvedFileName);
-            }
-
-            Assert.Equal(approvedPublicApi, receivedPublicApi);
-        }
-
-        private static string Filter(string text)
-        {
-            text = _removeCoverletSectionRegex.Replace(text, string.Empty);
-            return string.Join(Environment.NewLine, text.Split(
-                new[]
-                {
-                    Environment.NewLine
-                },
-                StringSplitOptions.RemoveEmptyEntries)
-                    .Where(l =>
-                    !l.StartsWith("[assembly: AssemblyVersion(", StringComparison.InvariantCulture) &&
-                    !l.StartsWith("[assembly: AssemblyFileVersion(", StringComparison.InvariantCulture) &&
-                    !l.StartsWith("[assembly: AssemblyInformationalVersion(", StringComparison.InvariantCulture) &&
-                    !string.IsNullOrWhiteSpace(l)));
-        }
-    }
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Fact]
+    public Task PunchclockTests() => typeof(OperationQueue).Assembly.CheckApproval(["Punchclock"]);
 }
